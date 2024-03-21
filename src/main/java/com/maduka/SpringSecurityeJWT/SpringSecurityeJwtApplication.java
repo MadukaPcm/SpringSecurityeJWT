6afd3bdf87fdcd05ea90a6851e2f060f365a347a
frank <frank.kessi@eganet.go.tz>
2024-03-21 22:43:16 +0300

    first commit

---
+++ src/main/java/com/maduka/SpringSecurityeJWT/SpringSecurityeJwtApplication.java
@@ -0,0 +1,13 @@
+package com.maduka.SpringSecurityeJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityeJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityeJwtApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/maduka/SpringSecurityeJWT/repository/OurUserRepo.java
@@ -0,0 +1,10 @@
+package com.maduka.SpringSecurityeJWT.repository;
+
+import com.maduka.SpringSecurityeJWT.entity.OurUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OurUserRepo extends JpaRepository<OurUsers, Integer> {
+    Optional<OurUsers> findByEmail(String email);
+}
